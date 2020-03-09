213aa6fb790754ba0eb7d4d55f58ecd13f3d649d
Richard Stanners <richard.stanners@gmail.com>
2020-03-09 19:36:11 +1300

    beta v0.1

--- config_prd.py
+++
@@ -1,9 +0,0 @@
-hubspot = {
-            'api'                       : '12e61567-f79c-4fa7-984f-f5e959d457b9',
-            'getOwnerEndPoint'          : 'https://api.hubapi.com/owners/v2/owners/',
-            'getDealEndPoint'           : 'https://api.hubapi.com/deals/v1/deal/',
-            'getRecentDealsEndPoint'    : 'https://api.hubapi.com/deals/v1/deal/recent/modified',
-            'dealRetreiveCount'         : '100',
-            'offlineDealPipeline'   : '1061525',
-            'OfflineClosedWon'      : 'OFF-03'
-        }
